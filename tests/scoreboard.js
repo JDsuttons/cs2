4abe87609f38feb54a5f86bcce61ec52dbdf7111
JDsuttons <joe.d.sutton@gmail.com>
2025-03-11 15:20:31 +0000

    csv parser working

--- tests/scoreboard.js
+++ tests/scoreboard.js
@@ -3,10 +3,11 @@ const path = require("path");
 const directory = path.join(__dirname,'\\outputs');
 
 var {parseEvent, parseTicks} = require('@laihoe/demoparser2');
+
 const pathToDemo = "\\demos\\natus-vincere-vs-apeks-m3-mirage-p2.dem"
 
 let gameEndTick = Math.max(...parseEvent(pathToDemo,"round_end").map(x => x.tick))
-let fields = ["ace_rounds_total","deaths_total","headshot_kills_total","kills_total","mvps","score"]
+let fields = ["4k_rounds_total","ace_rounds_total","deaths_total","headshot_kills_total","kills_total","mvps","score","team_clan_name", "team_rounds_total"]
 let scoreboard = parseTicks(pathToDemo, fields, [gameEndTick])
 
 let header = fields.join(","); 
