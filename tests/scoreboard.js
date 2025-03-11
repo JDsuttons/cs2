8b4a47fd15a699ee4a2df65055e1418de42df4c2
JDsuttons <joe.d.sutton@gmail.com>
2025-03-11 17:05:03 +0000

    working dir

--- tests/scoreboard.js
+++ tests/scoreboard.js
@@ -4,11 +4,11 @@ const directory = path.join(__dirname,'\\outputs');
 
 var {parseEvent, parseTicks} = require('@laihoe/demoparser2');
 
-const pathToDemo = "\\demos\\natus-vincere-vs-apeks-m3-mirage-p2.dem"
+const data = fs.readFileSync("\\Work\\repos\\cs2\\demos\\complexity-vs-vitality-m1-inferno.dem")
 
-let gameEndTick = Math.max(...parseEvent(pathToDemo,"round_end").map(x => x.tick))
+let gameEndTick = Math.max(...parseEvent(data,"round_end").map(x => x.tick))
 let fields = ["4k_rounds_total","ace_rounds_total","deaths_total","headshot_kills_total","kills_total","mvps","score","team_clan_name", "team_rounds_total"]
-let scoreboard = parseTicks(pathToDemo, fields, [gameEndTick])
+let scoreboard = parseTicks(data, fields, [gameEndTick])
 
 let header = fields.join(","); 
 let content = scoreboard.map(scoreboard => 
