5721d51cac59638976f80e9bc87fd701816cb5cf
JDsuttons <joe.d.sutton@gmail.com>
2025-03-11 17:01:03 +0000

    housekeeping

--- tests/chickenkills.js
+++
@@ -1,8 +0,0 @@
-var {parseEvent} = require('@laihoe/demoparser2');
-
-
-// make sure your demo has chickens
-let events = parseEvent("\\demos\\natus-vincere-vs-apeks-m3-mirage-p2.dem", "other_death")
-let chickenKills = events.filter(event => event.othertype == "chicken")
-
-console.log(chickenKills)
