80de060ccb56f3fb036a94b8b5d2ba1a0c225ce0
JDsuttons <joe.d.sutton@gmail.com>
2025-03-11 13:19:34 +0000

    actual working csv parse pog

--- tests/random.js
+++ tests/random.js
@@ -1,7 +1,14 @@
 var {parseEvent} = require('@laihoe/demoparser2');
 const fs = require('fs');
+const path = require("path");
+const directory = path.join(__dirname,'\\outputs');
  
 const data = fs.readFileSync('\\demos\\natus-vincere-vs-apeks-m3-mirage-p2.dem');
 let buf = Buffer.from(data)
 let out = parseEvent(buf, "player_death", ["X", "Y"], ["total_rounds_played"])
+
+fs.writeFileSync(directory+"/newcsvtest.csv",'out');
+
 console.log(out)
+
+
--- tests/scoreboard.js
+++ tests/scoreboard.js
@@ -3,18 +3,17 @@ const path = require("path");
 const directory = path.join(__dirname,'\\outputs');
 
 var {parseEvent, parseTicks} = require('@laihoe/demoparser2');
-
 const pathToDemo = "\\demos\\natus-vincere-vs-apeks-m3-mirage-p2.dem"
 
 let gameEndTick = Math.max(...parseEvent(pathToDemo,"round_end").map(x => x.tick))
-let fields = ["kills_total", "deaths_total", "mvps", "headshot_kills_total", "ace_rounds_total", "score"]
+let fields = ["ace_rounds_total","deaths_total","headshot_kills_total","kills_total","mvps","score"]
 let scoreboard = parseTicks(pathToDemo, fields, [gameEndTick])
 
-let header = fields
-let content = scoreboard
+let header = fields.join(","); 
+let content = scoreboard.map(scoreboard => 
+  Object.values(scoreboard).join(",")
+);
+const csvContent = header + "\n" + content.join("\n")
 
-let csvContent = header+ "\n" +content.join(",")
-
-fs.writeFileSync(directory+"/testtest.csv",csvContent);
-
-console.log(scoreboard);
+fs.writeFileSync(directory+"/newcsvtest.csv",csvContent);
+console.log(scoreboard)
