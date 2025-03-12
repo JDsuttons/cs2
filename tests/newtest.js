ec3dfc9884531b83ef17caf0f6223643548e785c
JDsuttons <joe.d.sutton@gmail.com>
2025-03-12 18:02:38 +0000

    added inital helpers.js file for headers

--- tests/newtest.js
+++ tests/newtest.js
@@ -1,16 +1,18 @@
 const fs = require("fs")
 const path = require("path");
 const directory = path.join(__dirname,'\\outputs');
+const { Helpers } = require('\\Work\\repos\\cs2\\helpers')
 
 var {parseEvent} = require('@laihoe/demoparser2');
 const { kill } = require("process");
 
 const data = fs.readFileSync("\\Work\\repos\\cs2\\demos\\complexity-vs-vitality-m1-inferno.dem")
 
-let kills = parseEvent(data, "round_end")
+let kills = parseEvent(data, "player_death")
 
 
-let header = ["event_name","reason","round","tick","winner" ]
+var helpers = new Helpers()
+let header = helpers.playerDeathsHeader()
 
 let content = kills.map(kills => 
   Object.values(kills).join(",")
