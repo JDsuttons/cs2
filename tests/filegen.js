9d0a4be7b966820befd201af5cf9679273e33f0c
JDsuttons <joe.d.sutton@gmail.com>
2025-03-12 17:50:17 +0000

    housekeeping

--- tests/filegen.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs")
-const path = require("path");
-const directory = path.join(__dirname,'\\outputs');
-
-fs.writeFileSync(directory+"/testtest.csv","Hi this is a text file");
-
